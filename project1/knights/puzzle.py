a75815920118d11aa27e0ea8dc22684d0f51d213
gonzabaleta <gonza.zf@gmail.com>
2022-10-10 01:09:45 -0300

    finished Knights problem

--- project1/knights/puzzle.py
+++ project1/knights/puzzle.py
@@ -9,24 +9,41 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+knowledgeBase  = And(
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+)
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    knowledgeBase,
+    Biconditional(AKnight, And(AKnight, AKnave)), # If the sentence is true, then A is a Knight
+    Biconditional(AKnave, Not(And(AKnight, AKnave))), # If the sentence is false, then A is a Knave
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    knowledgeBase,
+    Biconditional(AKnight, And(AKnave, BKnave)), # If the sentence is true, then A is a Knight
+    Biconditional(AKnave, Not(And(AKnave, BKnave))), # If the sentence is false, then A is a Knave
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    knowledgeBase,
+    Implication(AKnight, BKnight), # If A is a knight, then B MUST be a Knight
+    Implication(AKnave, BKnight), # If A is a Knave, then B MUST be a Knight because A is lying
+    Implication(BKnight, AKnave), # If B is a knight, then A MUST be a knave
+    Implication(BKnave, AKnave) # If B is a Knave, then A MUST be a knave
 )
 
 # Puzzle 3
@@ -35,7 +52,15 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    knowledgeBase,
+    Implication(And(BKnight, AKnight), AKnave), # If what B says is true, and A is a Knight, then A is a Knave
+    Implication(And(BKnight, AKnave), AKnight),  # If what B says is true and A is a Knave, then A is a Knight
+    # Since what B says is a contradiction, B must be a knave
+    Biconditional(BKnight, CKnave), # If B is a knight, then C a knave
+    Biconditional(BKnave, CKnight), # If B is a knave, then C a knight
+    Biconditional(CKnight, AKnight), # If C is a knight, then B a knave
+    Biconditional(CKnave, AKnave), # if C is a knave, then B a knight
+
 )
 
 
